5f1afeaa6ac19849b6b9cec00c9b4b9c4f6992ff
joshunrau <joshua@joshuaunrau.com>
2022-03-22 00:40:51 -0400

    update

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #SBATCH --account=def-mlepage
-#SBATCH --cpus-per-task=16
-#SBATCH --mem-per-cpu=1.5G
+#SBATCH --ntasks=4
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=1G
 #SBATCH --time=1:00:00
 
 source venv/bin/activate
