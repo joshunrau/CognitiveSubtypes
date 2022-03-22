bfd24b02a0b20013cde37e125ceafda1450863f2
joshunrau <joshua@joshuaunrau.com>
2022-03-21 23:57:15 -0400

    update main

--- src/main.py
+++ src/main.py
@@ -1,10 +1,17 @@
+import os
+import pandas as pd
+
 from data.build import BiobankData
 from data.dataset import Dataset
 from models.classify import BestSVC, BestRandomForestClassifier, BestRidgeClassifier
 from models.cluster import BestKMeans
 
 
-CURRENT_CSV="/Users/joshua/Developer/CognitiveSubtypes/data/raw/current.csv"
+CURRENT_CSV = "/home/junrau/scratch/CognitiveSubtypes/data/current.csv"
+RESULTS_DIR = "/home/junrau/scratch/CognitiveSubtypes/results"
+
+#CURRENT_CSV = "/Users/joshua/Developer/CognitiveSubtypes/data/raw/current.csv"
+#RESULTS_DIR = "/Users/joshua/Developer/CognitiveSubtypes/results"
 
 def main():
 
@@ -21,18 +28,22 @@ def main():
         BestRidgeClassifier()
     ]
     
-    best_score = -1
-    best_classifier = None
-    
+    results = {}
+    n_fit, n_remain = 0, len(classifiers)
     for model in classifiers:
         model.fit(data)
         score = model.score(data)
-        if score > best_score:
-            best_score = score
-            best_classifier = model
-            print("Found new best classifier: " + str(model))
-            print("Score: " + str(score))
-    return best_classifier
+        results[str(model)] = {
+            model.score_func.__name__: score
+        }
+        print("Finished fitting model: " + str(model))
+        print(f"{model.score_func.__name__}: {score}")
+        n_fit += 1
+        n_remain -= 1
+        print(f"Models Fit: {n_fit}")
+        print(f"Models Remaining: {n_remain}\n")
+    results = pd.DataFrame.from_dict(results, orient='index')
+    results.to_csv(os.path.join(RESULTS_DIR, "model_fitting.csv"))
 
 if __name__ == "__main__":
     main()
